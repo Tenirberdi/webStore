f482f5c4bb2da9c696c683122a4d4d57bf3edcd5
TEk <tekukambarov@gmail.com>
2022-10-01 16:25:41 +0600

    just adding spring boot init project and make configuration with MySQL database

---
+++ src/main/java/com/example/webStore/Models/Product.java
@@ -0,0 +1,22 @@
+package com.example.webStore.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    private int quantity;
+
+}
---
+++ src/main/java/com/example/webStore/WebStoreApplication.java
@@ -0,0 +1,14 @@
+package com.example.webStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStoreApplication.class, args);
+	}
+
+}
